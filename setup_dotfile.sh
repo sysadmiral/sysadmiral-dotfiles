69e2e47445c6502548dd0a7735e27034233ecf1a
Amo Chumber <root.demon.net@gmail.com>
2016-09-21 23:35:00 +0100

    copy bindir

--- setup_dotfile.sh
+++ setup_dotfile.sh
@@ -1,1 +1,8 @@
-for dotfile in $(ls _*); do dotfilepath=$(realpath ${dotfile}); endfile=".${dotfile:1}"; ln -s -f ${dotfilepath} ~/${endfile}; done
+for dotfile in $(ls _*)
+  do
+  dotfilepath=$(realpath ${dotfile})
+  endfile=".${dotfile:1}"
+  ln -s -f ${dotfilepath} ~/${endfile}
+done
+
+[[ -d bin ]] && ln -s -f $(realpath bin) ~/bin || "woah fuckup"
