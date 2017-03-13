91951dbc5fa301a3a5cbc28c8bdea692bb34d8dc
sysadmiral <root.demon.net@gmail.com>
2017-03-13 22:10:03 +0000

    pre migration commit

--- setup_dotfile.sh
+++ setup_dotfile.sh
@@ -1,8 +1,14 @@
-for dotfile in $(ls _*)
-  do
-  dotfilepath=$(realpath ${dotfile})
-  endfile=".${dotfile:1}"
-  ln -s -f ${dotfilepath} ~/${endfile}
-done
+#!/usr/bin/env bash
 
-[[ -d bin ]] && ln -s -f $(realpath bin) ~/bin || "woah fuckup"
+[[ -z "$CLEANED" ]] && exec /usr/bin/env -i CLEANED=1 /bin/bash "$0" "$@"
+env
+
+#for dotfile in $(ls _*)
+#  do
+#  dotfilepath=$(realpath ${dotfile})
+#  endfile=".${dotfile:1}"
+#  ln -s -f ${dotfilepath} ~/${endfile}
+#done
+
+[[ -d bin ]] && \
+  for binfile in $(ls binln -s -f $(realpath bin) ~/bin || "woah fuckup"
